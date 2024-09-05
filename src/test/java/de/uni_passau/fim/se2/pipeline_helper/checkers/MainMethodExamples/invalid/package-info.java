fdcb318f23eed27e75c0f6b2e6e543ca02d05215
Michael Baumgartner <Michael-Baumgartner@posteo.de>
2024-09-05 12:35:26 +0200

    Add missing package infos
    To fix CheckStyle Warnings

---
+++ src/test/java/de/uni_passau/fim/se2/pipeline_helper/checkers/MainMethodExamples/invalid/package-info.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Pipeline Helper Contributors
+//
+// SPDX-License-Identifier: CC0-1.0
+
+/**
+ * Contains invalid main method classes for the checker.
+ */
+package de.uni_passau.fim.se2.pipeline_helper.checkers.MainMethodExamples.invalid;
