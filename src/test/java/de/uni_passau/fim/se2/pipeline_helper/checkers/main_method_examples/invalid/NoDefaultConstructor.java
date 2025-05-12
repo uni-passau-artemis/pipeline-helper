836d0e52bacda76e645235730299a52cbbff9368
Erik Stern <stern@fim.uni-passau.de>
2025-05-12 17:59:00 +0200

    Fix bug where the main method checker didn't verify a non-args not-private constructor exists for an instance main method.

--- src/test/java/de/uni_passau/fim/se2/pipeline_helper/checkers/main_method_examples/invalid/NoDefaultConstructor.java
+++
@@ -1,6 +0,0 @@
-package de.uni_passau.fim.se2.pipeline_helper.checkers.main_method_examples.invalid;
-
-public class NoDefaultConstructor {
-    public NoDefaultConstructor(String test) {}
-    public void main() {}
-}
---
+++ src/test/java/de/uni_passau/fim/se2/pipeline_helper/checkers/main_method_examples/invalid/PrivateConstructorWithInstanceMethod.java
@@ -0,0 +1,8 @@
+package de.uni_passau.fim.se2.pipeline_helper.checkers.main_method_examples.invalid;
+
+public class PrivateConstructorWithInstanceMethod {
+    private PrivateConstructorWithInstanceMethod() {}
+    public PrivateConstructorWithInstanceMethod(String test) {}
+
+    public void main() {}
+}
